6738fb0ce36e709175f497d56b69e627f3fe94dc
Royal <2511535131@qq.com>
2023-01-13 18:40:39 +0800

    Update editJS.js

--- editJS.js
+++ editJS.js
@@ -1,4 +1,9 @@
 let searchBox = document.querySelector('.searchBox')
+let headPortrait = document.querySelector('.headPortrait')
+let uploadBackground = document.querySelector('.uploadBackground')
+let backgroundImg = document.querySelector('.Background>img')
+let writeQuestion = document.querySelector('.writeQuestion')
+let primebody = document.querySelector('body')
 
 searchBox.children[0].addEventListener('focus', ()=> { //搜索框焦点事件
     searchBox.style.width = '490px'
@@ -16,3 +21,59 @@ searchBox.children[0].addEventListener('blur', ()=> { //搜索框焦点移除事
     searchBox.style.opacity = 0.6
     searchBox.style.border = 'none'
 })
+
+writeQuestion.addEventListener('click', ()=> { //提问
+    let questionsBox = document.createElement('div')
+    questionsBox.classList.add('questionBox')
+    questionsBox.innerHTML = `
+        <span></span>
+        <div>
+            <span>
+                <img src="./img/users'headProtrait.jpg" alt="">
+            </span>
+            <input type="text" placeholder="写下你的问题，准确地描述问题更容易得到解答">
+            <textarea placeholder="输入问题背景、条件等详细信息（选填）"></textarea>
+            <span>发布问题</span>
+            <span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                    <path fill-rule="evenodd" d="M18.22 19.28a.75.75 0 1 0 1.06-1.06L13.06 12l6.22-6.22a.75.75 0 0 0-1.06-1.06L12 10.94 5.78 4.72a.75.75 0 0 0-1.06 1.06L10.94 12l-6.22 6.22a.75.75 0 1 0 1.06 1.06L12 13.06l6.22 6.22Z" clip-rule="evenodd"></path>
+                </svg>
+            </span>
+        </div>
+    `
+    primebody.insertBefore(questionsBox, primebody.children[0])
+    questionsBox.children[1].children[4].addEventListener('click', ()=> { //点击叉叉
+        primebody.removeChild(primebody.children[0])
+    })
+    questionsBox.children[1].children[1].addEventListener('input', ()=> {   //输入问题
+        if(questionsBox.children[1].children[1].value != '') {
+            questionsBox.children[1].style.height = '350px'
+            questionsBox.children[1].children[2].style.opacity = 1
+            questionsBox.children[1].children[2].style.height = '100px'
+        }else {
+            questionsBox.children[1].style.height = '250px'
+            questionsBox.children[1].children[2].style.opacity = 0
+            questionsBox.children[1].children[2].style.height = '0px'
+        }
+    })
+})
+
+headPortrait.children[2].onchange = function () { //上传头像
+    let fileData = this.files[0]
+    let reader = new FileReader()
+    reader.readAsDataURL(fileData)
+    reader.onload = function(e) {
+        console.log(e)
+        headPortrait.children[0].setAttribute('src', this.result)
+    }
+}
+uploadBackground.children[2].onchange = function () { //上传背景
+    let fileData = this.files[0]
+    let reader = new FileReader()
+    reader.readAsDataURL(fileData)
+    reader.onload = function(e) {
+        console.log(e)
+        // backgroundImg.style.backgroundColor = 'none'
+        backgroundImg.setAttribute('src', this.result)
+    }
+}
