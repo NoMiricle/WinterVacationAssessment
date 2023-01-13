a2d0923fae71ad827da90912c4b4e3ee30d0bc84
Royal <2511535131@qq.com>
2023-01-13 13:05:41 +0800

    Update editJS.js

---
+++ editJS.js
@@ -0,0 +1,18 @@
+let searchBox = document.querySelector('.searchBox')
+
+searchBox.children[0].addEventListener('focus', ()=> { //搜索框焦点事件
+    searchBox.style.width = '490px'
+    searchBox.style.backgroundColor = 'white'
+    searchBox.children[0].style.backgroundColor = 'white'
+    searchBox.style.border = '1px solid black'
+    searchBox.style.opacity = 1
+    searchBox.nextElementSibling.style.visibility = 'hidden'
+})
+searchBox.children[0].addEventListener('blur', ()=> { //搜索框焦点移除事件
+    searchBox.style.width = '400px'
+    searchBox.nextElementSibling.style.visibility = 'visible'
+    searchBox.style.backgroundColor = '#d7d7d7'
+    searchBox.children[0].style.backgroundColor = '#d7d7d7'
+    searchBox.style.opacity = 0.6
+    searchBox.style.border = 'none'
+})
