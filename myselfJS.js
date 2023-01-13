4e16941d11f86c01d34f410e49254819a44e2387
Royal <2511535131@qq.com>
2023-01-13 18:40:51 +0800

    Update myselfJS.js

--- myselfJS.js
+++ myselfJS.js
@@ -5,6 +5,8 @@ let nameBox = document.querySelector('.nameBox')
 let headPortrait = document.querySelector('.headPortrait')
 let uploadBackground = document.querySelector('.uploadBackground')
 let backgroundImg = document.querySelector('.backgroundImg')
+let writeQuestion = document.querySelector('.writeQuestion')
+let primebody = document.querySelector('body')
 
 for(let i = 0; i < check.children.length; i++) {
     check.children[i].addEventListener('click', ()=> { //点击切换
@@ -47,6 +49,42 @@ searchBox.children[0].addEventListener('blur', ()=> { //搜索框焦点移除事
     searchBox.children[0].style.backgroundColor = '#d7d7d7'
     searchBox.style.opacity = 0.6
     searchBox.style.border = 'none'
+})
+
+writeQuestion.addEventListener('click', ()=> { //提问
+    let questionsBox = document.createElement('div')
+    questionsBox.classList.add('questionBox')
+    questionsBox.innerHTML = `
+        <span></span>
+        <div>
+            <span>
+                <img src="./img/users'headProtrait.jpg" alt="">
+            </span>
+            <input type="text" placeholder="写下你的问题，准确地描述问题更容易得到解答">
+            <textarea placeholder="输入问题背景、条件等详细信息（选填）"></textarea>
+            <span>发布问题</span>
+            <span>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+                    <path fill-rule="evenodd" d="M18.22 19.28a.75.75 0 1 0 1.06-1.06L13.06 12l6.22-6.22a.75.75 0 0 0-1.06-1.06L12 10.94 5.78 4.72a.75.75 0 0 0-1.06 1.06L10.94 12l-6.22 6.22a.75.75 0 1 0 1.06 1.06L12 13.06l6.22 6.22Z" clip-rule="evenodd"></path>
+                </svg>
+            </span>
+        </div>
+    `
+    primebody.insertBefore(questionsBox, primebody.children[0])
+    questionsBox.children[1].children[4].addEventListener('click', ()=> { //点击叉叉
+        primebody.removeChild(primebody.children[0])
+    })
+    questionsBox.children[1].children[1].addEventListener('input', ()=> {   //输入问题
+        if(questionsBox.children[1].children[1].value != '') {
+            questionsBox.children[1].style.height = '350px'
+            questionsBox.children[1].children[2].style.opacity = 1
+            questionsBox.children[1].children[2].style.height = '100px'
+        }else {
+            questionsBox.children[1].style.height = '250px'
+            questionsBox.children[1].children[2].style.opacity = 0
+            questionsBox.children[1].children[2].style.height = '0px'
+        }
+    })
 })
 
 check.children[0].click() //初始选中
