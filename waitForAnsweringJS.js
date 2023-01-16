8b447c9e3eeb1cd1827f747aeede65123df57204
Royal <2511535131@qq.com>
2023-01-16 21:37:08 +0800

    Update waitForAnsweringJS.js

--- waitForAnsweringJS.js
+++ waitForAnsweringJS.js
@@ -6,7 +6,7 @@ let problemDisplayBox = document.querySelector('.problemDisplayBox')
 
 
 if(!localStorage.getItem('uid')) { //如果未登录则回到登录界面
-    location.href = 'file:///D:/Learn/Web/LanshanWorks/WinterVacationAssessment/login.html'
+    location.href = './login.html'
 }
 
 let userImformation = JSON.parse(localStorage.getItem('userImformation'))
